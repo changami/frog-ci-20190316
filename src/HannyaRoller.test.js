8b9db2968b1ea97e850362698bf1297c27dc8e40
chan_gami <babyryo@msn.com>
2019-03-16 16:24:15 -0700

    :construction_worker: Code Climate連携設定を追加し、コメントアウトしていたテストを解禁してカバレッジを上げていく

--- src/HannyaRoller.test.js
+++ src/HannyaRoller.test.js
@@ -6,24 +6,24 @@ describe('HannyaRoller', () => {
     expect(1 + 2).toBe(3);
   });
 
-  // /** @type HannyaRoller */
-  // let roller;
-  //
-  // /** @type HTMLDivElement */
-  // let el;
-  //
-  // beforeEach(() => {
-  //   el = window.document.createElement('div');
-  //   Object.defineProperty(el, 'clientWidth', { value: 800 });
-  //   Object.defineProperty(el, 'clientHeight', { value: 600 });
-  //   roller = new HannyaRoller({ el, text });
-  //
-  //   roller.start();
-  // });
-  //
-  // it('sets up under the specified element', () => {
-  //   /** @type Element */
-  //   const elSpace = el.firstElementChild;
-  //   expect(elSpace.classList.contains('HannyaRoller-space')).toBeTruthy();
-  // });
+  /** @type HannyaRoller */
+  let roller;
+
+  /** @type HTMLDivElement */
+  let el;
+
+  beforeEach(() => {
+    el = window.document.createElement('div');
+    Object.defineProperty(el, 'clientWidth', { value: 800 });
+    Object.defineProperty(el, 'clientHeight', { value: 600 });
+    roller = new HannyaRoller({ el, text });
+
+    roller.start();
+  });
+
+  it('sets up under the specified element', () => {
+    /** @type Element */
+    const elSpace = el.firstElementChild;
+    expect(elSpace.classList.contains('HannyaRoller-space')).toBeTruthy();
+  });
 });
